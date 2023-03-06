30f2d17b2d98e70f90619cb70c2f5015d5135dae
JamesH <jwhusc7@gmail.com>
2023-03-06 12:56:23 -0500

    refactored index.html into handlebars files

---
+++ controllers/home-routes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment, Vote } = require('../models');
+
+// get all posts for homepage
+router.get('/', (req, res) => {
+      res.render('index');
+});
+
+// login page
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
